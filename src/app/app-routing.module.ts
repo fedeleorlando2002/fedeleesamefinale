8d8cecefb53bff307758f05d0d2ac2667311d843
fedeleorlando2002 <fedeleorlando056@gmail.com>
2024-03-27 14:09:12 +0100

    app.routing modifica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,46 @@ import { SidebarComponent } from './pages/sidebar/sidebar.component';
 
 
 const routes: Routes = [
-  { path: '', component: SidebarComponent},
-  { path: 'home', component: HomeComponent },
-  { path: 'libri', component: LibriComponent },
-  { path: 'clienti', component: ClientiComponent},
+  // { path: '', component: SidebarComponent},
+  // { path: 'home', component: HomeComponent },
+  // { path: 'libri', component: LibriComponent },
+  // { path: 'clienti', component: ClientiComponent},
+  {
+    path: '',
+    redirectTo: '/sidebar',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    // loadchildren serve per caricare un modulo in modo asincrono (quindi non appena viene caricata la pagina) e quindi più velocemente
+    loadChildren: () =>
+      import('./pages/home/home.module').then(
+        (m) => m.HomeModule
+      ),
+    component: HomeComponent,
+  },
+  {
+    path: 'sidebar',
+    loadChildren: () =>
+      import('./pages/sidebar/sidebar.module').then(
+        (m) => m.MySidebarModule
+      ),
+    component: SidebarComponent,
+  },
+  {
+    path: 'libri',
+    loadChildren: () =>
+      import('./pages/libri/libri.module').then(
+        (m) => m.LibriModule
+      ),
+    component: LibriComponent,
+  },
+  {
+    path: 'clienti',
+    loadChildren: () =>
+      import('./pages/clienti/clienti.module').then((m) => m.ClientiModule),
+    component: ClientiComponent,
+  },
 ]
 
 @NgModule({
