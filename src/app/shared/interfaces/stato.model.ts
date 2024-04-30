48dded5a91e9f2edeb7c1a0ea3e1924006e5cdfa
fedeleorlando2002 <fedeleorlando056@gmail.com>
2024-04-30 12:00:39 +0200

    creato il file stato model

---
+++ src/app/shared/interfaces/stato.model.ts
@@ -0,0 +1,12 @@
+export interface StatoRichiesta {
+    stato: number;
+    label: string;
+    colore: string;
+  }
+  
+  export const StaticStatoRichiesta: StatoRichiesta[] = [
+    { stato: 0, label: "meno 10 libri", colore: "red" },
+    { stato: 1, label: "più 10 libri", colore: "green" },
+    { stato: 2, label: "più 20 libri", colore: "blue" },
+  ];
+  
